46e55148824a688dcfe61dd39a77445f06aa1f8f
HikmaAnwar <Hikmaanwar89@gmail.com>
2025-02-19 10:11:46 +0300

    feat: remove padding from main content area in layout component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
       <body>
         <MantineProvider>
           <Navigation />
-          <main className="pt-20">{children}</main>{" "}
+          <main>{children}</main>{" "}
         </MantineProvider>
       </body>
     </html>
